0c97aff95edad25285856fbae4dc4d55ca1533cf
فواز منصور الشمري <fawazshmmri2@gmail.com>
2025-10-03 15:03:39 +0300

    Update tip_calculatior.py

--- tip_calculatior.py
+++ tip_calculatior.py
@@ -1,4 +1,4 @@
-print("مرحبا بك في برنامج حساب الفاتورة ! ")
+print(مرحبًا بك في برنامج تقسيم الفاتورة ! ")
 bill = float(input("كم مبلغ الفاتورة ؟ "))
 tip = int(input("كم تريد ان تعطي بقشيش 0 , 5 , 10 , 12 , 15 الخ ؟ "))
 persons = int(input("كم عددكم ؟ "))
@@ -7,5 +7,6 @@ Total_Calc_tip = bill * Calc_tip
 Calc_Bill_tip = (bill + Total_Calc_tip ) /  persons
 final_Bill = round(Calc_Bill_tip, 2)
 print(f"الحسبة على كل واحد {final_Bill} ")
+
 
 
